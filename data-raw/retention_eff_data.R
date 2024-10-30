737c14459c39ea26515b6980d869875825d56fbb
Taynara <taynara.fernandes@ufz.de>
2024-10-30 14:37:44 +0100

    Increase the number of years so the function has enough data to run the calculations

--- data-raw/retention_eff_data.R
+++ data-raw/retention_eff_data.R
@@ -8,5 +8,5 @@ ret_eff_data <- read.table(paste0(path,"/retention_eff.csv"), header=T, sep=",",
 # ret_eff_data$doy <- lubridate::yday(ret_eff_data$date)
 # ret_eff_data$year <- lubridate::year(ret_eff_data$date)
 ret_eff_data$date <- as.POSIXct(ret_eff_data$date,tz="UTC")
-ret_eff_data <- ret_eff_data[ret_eff_data$year %in% 2018:2020,]
+ret_eff_data <- ret_eff_data[ret_eff_data$year %in% 2005:2020,]
 usethis::use_data(ret_eff_data, overwrite = TRUE)
